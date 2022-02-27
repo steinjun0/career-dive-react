73e82392f0297c735f32fefc4e3b8bf6bd5049c7
deagwon97 <curiosity806@naver.com>
2022-02-27 18:04:32 +0900

    create

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,21 @@
+properties([pipelineTriggers([githubPush()])])
+node {
+    stage ('Pull scource from github'){
+        git branch: 'main',
+        credentialsId: 'git-ssh',
+        url: 'git@github.com/steinjun0/career-dive-react.git'
+    }
+    stage('Build image by kaniko') {
+        withKubeConfig([credentialsId: 'kube-config']) {
+        sh 'curl -LO "https://storage.googleapis.com/kubernetes-release/release/v1.20.5/bin/linux/amd64/kubectl"'  
+        sh 'chmod u+x ./kubectl'  
+        sh 'bash ./deploy/run-kaniko.sh'
+    }
+    }
+    stage ('Wait building'){
+        sh 'bash ./deploy/wait-kaniko.sh'
+    }
+    stage ('rollout deployment'){
+        sh './kubectl rollout restart deployment career-dive-react'
+    }
+}    
