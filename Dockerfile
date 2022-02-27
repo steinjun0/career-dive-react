bd0d1f4db7345642066640a884e46e3e46b5018f
deagwon97 <curiosity806@naver.com>
2022-02-27 17:57:57 +0900

    update

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,6 @@ RUN npm i --only=production
 RUN npm run build
 
 FROM nginx:latest AS careerdive-frontend
-COPY --from=builder ./src/build /src/build
+COPY --from=builder /root/src/build /src/build
 COPY ./deploy/front-nginx/front.conf \
     /etc/nginx/conf.d/default.conf
--- deploy/run-kaniko.sh
+++ deploy/run-kaniko.sh
@@ -2,12 +2,12 @@
 IFS=" "
 string="`./kubectl get pods --output=jsonpath={.items[*].metadata.name}`"
 array=( $string )
-value="kaniko-front"
+value="kaniko-cd-front"
 echo "${IFS}${array[*]}${IFS}"
 if [[ "${IFS}${array[*]}${IFS}" =~ "${IFS}${value}${IFS}" ]]; then
     echo 'replace existed pod' 
-    ./kubectl replace --force -f ./deploy/kaniko-front.yaml
+    ./kubectl replace --force -f ./deploy/kaniko.yaml
 else 
     echo 'create' 
-    ./kubectl create -f ./deploy/kaniko-front.yaml
+    ./kubectl create -f ./deploy/kaniko.yaml
 fi
