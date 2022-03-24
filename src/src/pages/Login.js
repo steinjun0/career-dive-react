40d0c6bbf922ecd50ded45f79187bdeb29352724
steinjun0 <1142308@naver.com>
2022-03-24 14:43:38 +0000

    add Login API(not finish by CORS)

--- src/src/pages/Login.js
+++ src/src/pages/Login.js
@@ -1,3 +1,4 @@
+import API from '../API.js'
 import { Grid, styled, TextField } from "@mui/material";
 
 import {
@@ -12,6 +13,7 @@ import {
     colorTextLight,
     CustomButton
 } from "../util/styledComponent";
+import { useState } from 'react';
 
 
 const LoginWrapper = styled(VerticalFlex)`
@@ -34,7 +36,15 @@ const ButtonWrapper = styled(VerticalFlex)`
   margin-top: 20px;
 `
 
+const postLogin = (email, password) => {
+    console.log(email);
+    API.postLogin(email, password);
+}
+
 function App() {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
     return (
         <FullWidthWrapper>
             <MaxWidthDiv>
@@ -46,11 +56,11 @@ function App() {
                                     로그인
                                 </TextHeading6>
                                 <TextFieldWrapper>
-                                    <TextField variant="filled" InputProps={{ disableUnderline: true, }} fullWidth={true} margin="dense" size="small" hiddenLabel placeholder="이메일" />
-                                    <TextField variant="filled" InputProps={{ disableUnderline: true, }} fullWidth={true} margin="dense" size="small" hiddenLabel placeholder="비밀번호" />
+                                    <TextField onChange={(event) => { setEmail(event.target.value) }} variant="filled" InputProps={{ disableUnderline: true, }} fullWidth={true} margin="dense" size="small" hiddenLabel placeholder="이메일" />
+                                    <TextField onChange={(event) => { setPassword(event.target.value) }} variant="filled" InputProps={{ disableUnderline: true, }} fullWidth={true} margin="dense" size="small" hiddenLabel placeholder="비밀번호" />
                                 </TextFieldWrapper>
                                 <ButtonWrapper>
-                                    <CustomButton height="50px">로그인</CustomButton>
+                                    <CustomButton onClick={() => { postLogin(email, password) }} height="50px">로그인</CustomButton>
                                 </ButtonWrapper>
                             </VerticalFlex>
                         </LoginWrapper>
