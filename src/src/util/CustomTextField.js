2acb8eb3054b6a4c5f8481fb71a30b94ee1d516c
steinjun0 <1142308@naver.com>
2022-04-17 07:04:50 +0000

    CustomTextField component화 시킴

---
+++ src/src/util/CustomTextField.js
@@ -0,0 +1,12 @@
+import { TextField, styled } from "@mui/material";
+
+export const CustomTextField = styled(TextField)`
+  .MuiFilledInput-root{
+    border-radius: 8px;
+  }
+  .MuiFilledInput-input{
+    height: 32px;
+    padding: 10px 20px;
+    border-radius: 8px;
+  }
+`;
