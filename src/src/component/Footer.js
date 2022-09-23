5036942abd6a6f0b705612a3c285931648523a31
steinjun0 <1142308@naver.com>
2022-09-23 15:42:45 +0000

    footer disable url 추가(login, signup)

--- src/src/component/Footer.js
+++ src/src/component/Footer.js
@@ -1,4 +1,5 @@
 import { Divider, styled } from "@mui/material";
+import { useLocation } from "react-router-dom";
 import {
   RowAlignCenterFlex,
   Flex,
@@ -82,39 +83,46 @@ const SpeechBubbleIcon = styled('img')`
   margin-right: 6px;
 `;
 
+const footerDisableUrl = ['/login', '/signup']
+
 function Footer() {
+  const { pathname } = useLocation();
+
   return (
-    <FooterFullWidthWrapper>
-      <FooterTop>
-        <FooterWrapper>
-          <ColumnSet>
-            <ColumnTitle>COMPANY</ColumnTitle>
-            <div>기업소개</div>
-            <div>공지사항</div>
-          </ColumnSet>
-          <ColumnSet>
-            <ColumnTitle>POLICY</ColumnTitle>
-            <div>이용약관</div>
-            <div>개인정보처리방침</div>
-          </ColumnSet>
-          <ColumnSet>
-            <ColumnTitle>SUPPORT</ColumnTitle>
-            <div>FAQ</div>
-            <div>꾸다센터</div>
-          </ColumnSet>
-          <ContactCCudaButton>
-            <SpeechBubbleIcon src={speechBubble} alt='speech-bubble' />
-            문의하기
-          </ContactCCudaButton>
-        </FooterWrapper>
-      </FooterTop>
-      <FooterBottom>
-        <FooterBottomWrapper>
-          <div>artisd.studio, 사업자 등록 번호: 876-22-01034, 대표자: 김인종</div>
-          <div>Copyright 2022, 커리어다이브, All Rights Reserved.</div>
-        </FooterBottomWrapper>
-      </FooterBottom>
-    </FooterFullWidthWrapper>
+    <Flex >
+      {!footerDisableUrl.includes(pathname) && <FooterFullWidthWrapper>
+        <FooterTop>
+          <FooterWrapper>
+            <ColumnSet>
+              <ColumnTitle>COMPANY</ColumnTitle>
+              <div>기업소개</div>
+              <div>공지사항</div>
+            </ColumnSet>
+            <ColumnSet>
+              <ColumnTitle>POLICY</ColumnTitle>
+              <div>이용약관</div>
+              <div>개인정보처리방침</div>
+            </ColumnSet>
+            <ColumnSet>
+              <ColumnTitle>SUPPORT</ColumnTitle>
+              <div>FAQ</div>
+              <div>꾸다센터</div>
+            </ColumnSet>
+            <ContactCCudaButton>
+              <SpeechBubbleIcon src={speechBubble} alt='speech-bubble' />
+              문의하기
+            </ContactCCudaButton>
+          </FooterWrapper>
+        </FooterTop>
+        <FooterBottom>
+          <FooterBottomWrapper>
+            <div>artisd.studio, 사업자 등록 번호: 876-22-01034, 대표자: 김인종</div>
+            <div>Copyright 2022, 커리어다이브, All Rights Reserved.</div>
+          </FooterBottomWrapper>
+        </FooterBottom>
+      </FooterFullWidthWrapper>}
+    </Flex>
+
   );
 }
 
