ea60f9b478c2f4839b90ae4eb6efc3774f625f59
steinjun0 <1142308@naver.com>
2022-09-24 07:31:34 +0000

    textField디자인 고정한 컴포넌트 생성

---
+++ src/src/util/Custom/CustomTextField1.js
@@ -0,0 +1,23 @@
+import { Flex } from "util/styledComponent";
+import { CustomTextField } from "./CustomTextField";
+
+function CustomTextField1({ onChange, onEnter, placeholder, fullWidth, value }) {
+  return <CustomTextField
+    onKeyPress={(e) => {
+      if (e.code === 'Enter') {
+        onEnter();
+      }
+    }}
+    value={value}
+    onChange={onChange}
+    variant="filled"
+    InputProps={{ disableUnderline: true, }}
+    fullWidth={fullWidth}
+    margin="dense"
+    size="small"
+    hiddenLabel
+    placeholder={placeholder}
+  />;
+}
+
+export default CustomTextField1;
