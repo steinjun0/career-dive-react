335602c3ac934e707d7665e2cfadf82b5aa84b9e
steinjun0 <1142308@naver.com>
2022-10-13 05:59:33 +0000

    멘토 검색페이지 api 연결

--- src/src/pages/Search.js
+++ src/src/pages/Search.js
@@ -3,14 +3,47 @@ import JobCategoryGroup from "component/home/JobCategoryGroup";
 import FamousMentorGroup from "component/home/FamousMentorGroup";
 import BottomEventGroup from "component/home/BottomEventGroup";
 
-import { GrayBackground, MaxWidthDiv, VerticalFlex } from "util/styledComponent";
+import { Flex, GrayBackground, MaxWidthDiv, VerticalFlex } from "util/styledComponent";
 import MentorCard from "component/mentor/MentorCard";
+import { useEffect, useState } from "react";
+import API from "API";
 
 function Search() {
+  const [mentorList, setMentorList] = useState()
+  useEffect(() => {
+    API.getAccountMentorList().then((res) => {
+      if (res.status === 200) {
+        setMentorList(res.data.Results)
+      }
+    })
+  }, [])
+
+
   return (
     <VerticalFlex>
       <GrayBackground>
-        <MentorCard
+        <Flex>
+          {mentorList && [...mentorList].map((mentorData, index) => {
+            return <Flex style={{ margin: 15 }}>
+              <MentorCard
+                key={index}
+                company={mentorData.CompName}
+                department={mentorData.DivisInComp}
+                job={mentorData.JobInComp}
+                name={"Sarah"}
+                inJob={mentorData.InService ? "현직자" : "경력자"}
+                duration={"기간"}
+                rating={4.5}
+                userId={mentorData.UserID}
+                isShowRating={false}
+                isShowTag={true} />
+            </Flex>
+
+          }
+          )}
+        </Flex>
+
+        {/* <MentorCard
           company={"LF"}
           department={"서비스 기획자"}
           job={"UX 리서처"}
@@ -20,7 +53,7 @@ function Search() {
           rating={4.5}
           userId={1}
           isShowRating={false}
-          isShowTag={true} />
+          isShowTag={true} /> */}
       </GrayBackground>
     </VerticalFlex>
   );
