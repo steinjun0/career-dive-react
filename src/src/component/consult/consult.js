f0450edcbb3c2b2e25d4c653d1dfb0fe0ee62f6f
steinjun0 <1142308@naver.com>
2022-11-11 16:23:52 +0900

    session 조작 아이콘 변경

--- src/src/component/consult/consult.js
+++ src/src/component/consult/consult.js
@@ -5,7 +5,7 @@ export function onEnterSession({ navigater, date, startTime, endTime, consultId 
   if (nowTime - startDateTime <= 0) {
     alert('상담이 시작되지 않았습니다')
     navigater(`/session/${consultId}`)
-  } else if (endDateTime - nowTime <= 0) {
+  } else if (endDateTime - nowTime <= 0) { // 5분59초 이후 진입 불가. 
     alert('상담 시간이 지났습니다')
     navigater(`/session/${consultId}`)
   }
