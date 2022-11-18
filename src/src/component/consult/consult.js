7216fde13486a7f75ab91b95dd66085c7d7acb34
steinjun0 <1142308@naver.com>
2022-11-18 23:24:51 +0900

    세션 입장시 새탭 열기

--- src/src/component/consult/consult.js
+++ src/src/component/consult/consult.js
@@ -10,13 +10,16 @@ export function onEnterSession({ navigater, date, consultStatus, startTime, endT
     const nowTime = new Date().getTime()
     if (nowTime - startDateTime <= 0) {
       alert('상담이 시작되지 않았습니다')
-      navigater(`/session/${consultId}`)
+      window.open(`/session/${consultId}`, '_blank');
+      // navigater(`/session/${consultId}`)
     } else if (endDateTime - nowTime <= 0) { // 5분59초 이후 진입 불가. 
       alert('지난 상담입니다')
-      navigater(`/session/${consultId}`)
+      window.open(`/session/${consultId}`, '_blank');
+      // navigater(`/session/${consultId}`)
     }
     else {
-      navigater(`/session/${consultId}`)
+      window.open(`/session/${consultId}`, '_blank');
+      // navigater(`/session/${consultId}`)
     }
   } else {
     alert(`상담 상태에 문제가 있습니다. 관리자에게 문의해주세요 ${consultStatus}`,)
