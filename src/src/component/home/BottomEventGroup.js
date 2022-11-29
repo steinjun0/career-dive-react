e228210cb352d699dc3c7d0e0b555eba85f897f0
steinjun0 <1142308@naver.com>
2022-11-29 13:05:03 +0000

    홈 디자인 변경 - 가이드라인, 카카오톡 채널

--- src/src/component/home/BottomEventGroup.js
+++ src/src/component/home/BottomEventGroup.js
@@ -1,5 +1,7 @@
 import { styled } from "@mui/material";
-import { colorCareerDiveBlue, Flex, RowAlignCenterFlex, TextHeading5 } from 'util/styledComponent'
+import { colorCareerDiveBlue, colorTextLight, Flex, RowAlignCenterFlex, TextHeading5, VerticalFlex } from 'util/styledComponent'
+import GuideLineBook from "assets/img/home/GuidelineBook.svg";
+import KakaotalkChannel from "assets/img/home/KakaotalkChannel.svg";
 
 const BottomEventCardsWrapper = styled(RowAlignCenterFlex)`
     justify-content: space-between;
@@ -13,10 +15,12 @@ function BottomEventGroup() {
     return (
         <BottomEventCardsWrapper>
             <Flex style={{
-                backgroundColor: colorCareerDiveBlue, borderRadius: 8,
+                justifyContent: 'space-between',
+                backgroundColor: 'white', borderRadius: 8,
                 padding: 24, marginRight: '12px',
-                width: '100%', height: '202px',
-                cursor: 'pointer'
+                width: '100%', height: '112px',
+                cursor: 'pointer',
+                boxShadow: '10px 20px 40px rgba(130, 130, 130, 0.1)'
             }}
                 onClick={
                     () => {
@@ -24,21 +28,37 @@ function BottomEventGroup() {
                     }
                 }
             >
-                <TextHeading5 color="white" style={{ textDecoration: 'underline' }}>서비스 정책 및 가이드라인 멘티편 ></TextHeading5>
+                <VerticalFlex>
+                    <TextHeading5 color={colorTextLight}>서비스 정책 및 가이드라인</TextHeading5>
+                    <TextHeading5 color={colorCareerDiveBlue}>멘티편</TextHeading5>
+                </VerticalFlex>
+                <VerticalFlex
+                    style={{ paddingTop: '24px' }}>
+                    <img src={GuideLineBook} alt="" />
+                </VerticalFlex>
             </Flex>
             <Flex style={{
-                backgroundColor: colorCareerDiveBlue, borderRadius: 8,
-                padding: 24, marginLeft: '12px',
-                width: '100%', height: '202px',
+                justifyContent: 'space-between',
+                backgroundColor: 'white', borderRadius: 8,
+                padding: 24, marginRight: '12px',
+                width: '100%', height: '112px',
                 cursor: 'pointer',
-
+                boxShadow: '10px 20px 40px rgba(130, 130, 130, 0.1)'
             }}
                 onClick={
                     () => {
                         window.open('https://pf.kakao.com/_xhHtxlxj')
                     }
-                }>
-                <TextHeading5 color="white" style={{ textDecoration: 'underline' }}>카카오톡 채널 문의</TextHeading5>
+                }
+            >
+                <VerticalFlex>
+                    <TextHeading5 color={'#F3C72B'}>카카오톡 채널 문의</TextHeading5>
+
+                </VerticalFlex>
+                <VerticalFlex
+                    style={{ paddingTop: '66px' }}>
+                    <img src={KakaotalkChannel} alt="" />
+                </VerticalFlex>
             </Flex>
         </BottomEventCardsWrapper>
     );
