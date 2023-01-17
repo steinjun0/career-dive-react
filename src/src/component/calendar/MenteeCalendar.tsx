c87362002700b42d2be5f6af94588a18e4079bad
steinjun0 <1142308@naver.com>
2023-01-17 09:23:22 +0000

    MenteeCalendar txs 생성중 - ts 변환 Card component 생성함

---
+++ src/src/component/calendar/MenteeCalendar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "../../util/ts/Card";
+
+const MenteeCalendar = () => {
+    return (
+        <Card
+            title='상담 가능 일정'
+            no_divider={true}
+        >
+        </Card>
+    );
+};
+export default MenteeCalendar
