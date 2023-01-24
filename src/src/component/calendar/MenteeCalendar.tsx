513434e32bf38afb51d3ee129151af9b05e554a4
steinjun0 <1142308@naver.com>
2023-01-24 10:01:39 +0000

    주석처리 및 코드 정리

--- src/src/component/calendar/MenteeCalendar.tsx
+++ src/src/component/calendar/MenteeCalendar.tsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { colorCareerDiveBlue, colorTextDisabled, colorTextLight, Flex } from "util/styledComponent";
+import { colorBackgroundGrayMedium, colorCareerDiveBlue, colorTextDisabled, colorTextLight, Flex } from "util/styledComponent";
 import Card from "../../util/ts/Card";
 import { getDatesOfMonth, isPastDate, monthList } from "./Calendar.service";
 import SimpleSelect from "util/ts/SimpleSelect";
 import API from "API";
+
+const dateDefaultStyle = {
+    justifyContent: 'center', alignItems: 'center', borderRadius: '12px', minHeight: '32px'
+}
 
 const MenteeCalendar = ({ userId }: { userId: number }) => {
     // Setting
@@ -17,16 +21,19 @@ const MenteeCalendar = ({ userId }: { userId: number }) => {
     interface IavailableTime {
         [key: number]: { startTime: Date, endTime: Date, ruleType: string, ruleId: number, scheduleId: number }[]
     }
-    const [availableTimes, setAvailableTimes] = useState<IavailableTime>({})
+    const [availableTimes, setAvailableTimes] = useState<IavailableTime>({}) // available의 date를 key값으로 정보를 가진 object
 
     const koDtf = Intl.DateTimeFormat("ko", { year: 'numeric', month: 'narrow' })
     const monthTextList = monthList.map(e => koDtf.format(e))
 
     useEffect(() => {
+        // 월별 날짜 표기
         const temp = getDatesOfMonth(currentYearAndMonth)
         const startDay = temp[0].getDay()
         const endDay = temp[temp.length - 1].getDay()
         setCalendarDates([...Array(startDay).fill(null), ...temp, ...Array(6 - endDay).fill(null)])
+
+        // 선택 가능 날짜 데이터 받아오고, state 설정하기
         setAvailableDates([])
         setAvailableTimes([])
         API.getConsultSchedule(currentYearAndMonth.getFullYear(), currentYearAndMonth.getMonth() + 1, userId)
@@ -58,6 +65,7 @@ const MenteeCalendar = ({ userId }: { userId: number }) => {
     }, [currentYearAndMonth])
 
     useEffect(() => {
+        // 선택 가능날짜 갱신될 때, 값이 있다면 첫 번째 값으로 날짜 자동 선택
         if (availableDates[0]) {
             setSelectedDate(availableDates[0])
         } else {
@@ -80,34 +88,30 @@ const MenteeCalendar = ({ userId }: { userId: number }) => {
                     />
                 </Flex>
             </Flex>
-            <Flex style={{ flexWrap: 'wrap', height: '344px' }}>
+            <Flex style={{ flexWrap: 'wrap', height: '344px', borderBottom: `1px solid ${colorBackgroundGrayMedium}`, paddingBottom: '16px' }}>
                 {
                     ['일', '월', '화', '수', '목', '금', '토'].map((koDay, i) => {
-                        return <Flex style={{ width: '14.286%', justifyContent: 'center' }} key={i}>{koDay}</Flex>
+                        return <Flex style={{ width: '14.286%', ...dateDefaultStyle }} key={i}>{koDay}</Flex>
                     })
                 }
                 {
                     calendarDates && calendarDates.map((date, i) => {
                         if (date === null) {
-                            return <Flex data-testid="date-elem" style={{ minWidth: '14.286%', minHeight: '32px', justifyContent: 'center', alignItems: 'center' }} key={i}></Flex>
+                            return <Flex data-testid="date-elem" style={{ minWidth: '14.286%', ...dateDefaultStyle }} key={i}></Flex>
                         }
                         else {
                             //비활성
                             if (isPastDate(date)) {
                                 return <Flex
-                                    data-testid="date-elem"
-                                    style={{ width: '14.286%', minHeight: '32px', justifyContent: 'center', alignItems: 'center', color: colorTextDisabled }}
-                                    key={i}>{date.getDate()}</Flex>
+                                    data-testid="date-elem" style={{ width: '14.286%', ...dateDefaultStyle, color: colorTextDisabled }} key={i}>{date.getDate()}
+                                </Flex>
                             }
                             // 선택
                             else if (selectedDate && selectedDate.getTime() === date.getTime()) {
                                 return <Flex
-                                    data-testid="date-elem"
-                                    style={{ width: '14.286%', justifyContent: 'center', alignItems: 'center', color: 'white' }}
-                                    key={i}>
+                                    data-testid="date-elem" style={{ width: '14.286%', ...dateDefaultStyle }} key={i}>
                                     <Flex
-                                        style={{ width: '32px', height: '32px', justifyContent: 'center', alignItems: 'center', backgroundColor: colorCareerDiveBlue, cursor: 'pointer', borderRadius: '12px' }}
-                                    >
+                                        style={{ width: '32px', ...dateDefaultStyle, color: 'white', backgroundColor: colorCareerDiveBlue, cursor: 'pointer' }}>
                                         {date.getDate()}
                                     </Flex>
 
@@ -116,10 +120,8 @@ const MenteeCalendar = ({ userId }: { userId: number }) => {
                             // 선택가능 
                             else if (availableDates.map(e => e.getTime()).includes(date.getTime())) {
                                 return <Flex
-                                    data-testid="date-elem"
-                                    style={{ width: '14.286%', justifyContent: 'center', alignItems: 'center', color: colorTextLight }}
-                                    key={i}>
-                                    <Flex style={{ width: '32px', height: '32px', justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgb(240, 240, 240)', cursor: 'pointer', borderRadius: '12px' }}
+                                    data-testid="date-elem" style={{ width: '14.286%', ...dateDefaultStyle }} key={i}>
+                                    <Flex style={{ width: '32px', ...dateDefaultStyle, color: colorTextLight, backgroundColor: 'rgb(240, 240, 240)', cursor: 'pointer' }}
                                         onClick={() => { setSelectedDate(date) }}>
                                         {date.getDate()}
                                     </Flex>
@@ -129,9 +131,9 @@ const MenteeCalendar = ({ userId }: { userId: number }) => {
                             // 일반
                             else {
                                 return <Flex
-                                    data-testid="date-elem"
-                                    style={{ width: '14.286%', minHeight: '32px', justifyContent: 'center', alignItems: 'center', color: colorTextLight }}
-                                    key={i}>{date.getDate()}</Flex>
+                                    data-testid="date-elem" style={{ width: '14.286%', ...dateDefaultStyle, color: colorTextLight }} key={i}>
+                                    {date.getDate()}
+                                </Flex>
                             }
                         }
                     })
