96f594ff0c8ffc8ceed5f166cd32531d08b92ae7
steinjun0 <1142308@naver.com>
2023-02-16 07:58:50 +0000

    signup2 1/3 구현 완료

--- src/src/pages/signup2.tsx
+++ src/src/pages/signup2.tsx
@@ -1,21 +1,51 @@
-import { Box, Divider, TextField } from "@mui/material";
+import { Box, Divider, styled, TextField } from "@mui/material";
 import { TextFieldWrapper } from "component/myPage/MenteeIntroduce";
 import React, { useEffect, useState } from "react";
 import { CustomButton } from "util/Custom/CustomButton";
 import { CustomCheckbox } from "util/Custom/CustomCheckbox";
 import { CustomPasswordTextField } from "util/Custom/CustomPasswordTextField";
 import CustomTextField from "util/ts/Custom/CustomTextField";
-import { CenterFlex, colorBackgroundGrayMedium, colorTextLight, EmptyHeight, Flex, RowAlignCenterFlex, TextCaption, TextHeading6, VerticalFlex } from "util/styledComponent";
+import { CenterFlex, colorBackgroundGrayMedium, colorCareerDiveBlue, colorTextLight, EmptyHeight, Flex, RowAlignCenterFlex, TextBody2, TextCaption, TextHeading6, VerticalFlex } from "util/styledComponent";
 import SignupTemplate from "./signup/SignupTemplate";
 import { validateEmail, validatePassword } from "util/ts/util";
 
-
+const TermsButton = styled(Flex)({
+  justifyContent: 'center',
+  alignItems: 'center',
+  border: `1px solid ${colorBackgroundGrayMedium}`,
+  borderRadius: '2px',
+  width: '39px',
+  height: '20px',
+  fontSize: '10px',
+  color: colorTextLight,
+  cursor: 'pointer'
+})
 
 export default function Signup2() {
   const [email, setEmail] = useState<string>('')
   const [isEmailValid, setIsEmailValid] = useState<boolean>(true)
   const [password, setPassword] = useState<string>('')
   const [isPasswordValid, setIsPasswordValid] = useState<boolean>(true)
+
+  const [isCheckUsingTerm, setIsCheckUsingTerm] = useState(false);
+  const [isCheckPersonalData, setIsCheckPersonalData] = useState(false);
+  const [isCheckMarketing, setIsCheckMarketing] = useState(false);
+  const [isCheckAll, setIsCheckAll] = useState(false);
+
+  function toggleAll() {
+    if (isCheckUsingTerm &&
+      isCheckPersonalData &&
+      isCheckMarketing) {
+      setIsCheckUsingTerm(false)
+      setIsCheckPersonalData(false)
+      setIsCheckMarketing(false)
+    }
+    else {
+      setIsCheckUsingTerm(true)
+      setIsCheckPersonalData(true)
+      setIsCheckMarketing(true)
+    }
+  }
 
   useEffect(() => {
     if (email === '') {
@@ -26,7 +56,6 @@ export default function Signup2() {
   }, [email])
 
   useEffect(() => {
-    console.log(password)
     if (password === '') {
       setIsPasswordValid(true)
     } else {
@@ -34,16 +63,17 @@ export default function Signup2() {
     }
   }, [password])
 
+  useEffect(() => {
+    if (isCheckUsingTerm && isCheckPersonalData && isCheckMarketing) {
+      setIsCheckAll(true)
+    } else {
+      setIsCheckAll(false)
+    }
+  }, [isCheckUsingTerm, isCheckPersonalData, isCheckMarketing])
+
   return <SignupTemplate title="회원가입" step="1/3">
-    <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <div style={{ height: 72 }}>
+    <VerticalFlex>
+      <section style={{ height: 72 }}>
         <CustomTextField
           onChange={(event) => { setEmail(event.target.value) }}
           placeholder="이메일"
@@ -51,8 +81,8 @@ export default function Signup2() {
           helperText={!isEmailValid ? "올바른 형식으로 입력해주세요" : undefined}
           height="48px"
         />
-      </div>
-      <VerticalFlex style={{ marginTop: '0' }}>
+      </section>
+      <section style={{ marginTop: '0' }}>
         <TextCaption style={{ marginLeft: '4px' }}>
           영문, 숫자, 특수문자 포함 8자 이상
         </TextCaption>
@@ -67,8 +97,64 @@ export default function Signup2() {
             type="password"
           />
         </div>
-      </VerticalFlex>
+      </section>
 
-    </Box>
+      <section>
+        <Flex style={{ marginTop: '4px', justifyContent: 'space-between' }}>
+          <Flex>
+            <CustomCheckbox isChecked={isCheckUsingTerm} setIsChecked={setIsCheckUsingTerm} onClick={undefined} children={undefined} />
+            <TextBody2
+              style={{ marginLeft: 4, color: colorTextLight, cursor: 'pointer' }}
+              onClick={(e) => { setIsCheckUsingTerm(!isCheckUsingTerm) }}>
+              이용약관 <span style={{ color: colorCareerDiveBlue }}>(필수)</span>
+            </TextBody2>
+          </Flex>
+          <TermsButton>
+            약관
+          </TermsButton>
+        </Flex>
+        <Flex style={{ marginTop: '12px', justifyContent: 'space-between' }}>
+          <Flex>
+            <CustomCheckbox isChecked={isCheckPersonalData} setIsChecked={setIsCheckPersonalData} onClick={undefined} children={undefined} />
+            <TextBody2
+              style={{ marginLeft: 4, color: colorTextLight, cursor: 'pointer' }}
+              onClick={(e) => { setIsCheckPersonalData(!isCheckPersonalData) }}>
+              개인 정보 활용 동의 <span style={{ color: colorCareerDiveBlue }}>(필수)</span>
+            </TextBody2>
+          </Flex>
+          <TermsButton>
+            약관
+          </TermsButton>
+        </Flex>
+        <Flex style={{ marginTop: '12px', justifyContent: 'space-between' }}>
+          <Flex>
+            <CustomCheckbox isChecked={isCheckMarketing} setIsChecked={setIsCheckMarketing} onClick={undefined} children={undefined} />
+            <TextBody2
+              style={{ marginLeft: 4, color: colorTextLight, cursor: 'pointer' }}
+              onClick={(e) => { setIsCheckMarketing(!isCheckMarketing) }}>
+              개인 정보 활용 동의 (선택)
+            </TextBody2>
+          </Flex>
+          <TermsButton>
+            약관
+          </TermsButton>
+        </Flex>
+      </section>
+      <Divider style={{ margin: '12px 0', color: colorBackgroundGrayMedium }}></Divider>
+      <Flex>
+        <CustomCheckbox isChecked={isCheckAll} setIsChecked={setIsCheckAll} onClick={toggleAll} children={undefined} />
+        <TextBody2 style={{ marginLeft: 4, color: colorTextLight, cursor: 'pointer' }} onClick={(e) => { toggleAll() }}>전체 동의</TextBody2>
+      </Flex>
+
+      <CustomButton
+        style={{ marginTop: 24 }}
+        disabled={!isCheckPersonalData || !isCheckUsingTerm || email === '' || password === '' || !isEmailValid || !isPasswordValid}
+        height={'48px'}
+        onClick={() => {
+
+        }}>
+        다음
+      </CustomButton>
+    </VerticalFlex>
   </SignupTemplate>
 }
