8247292f6b4bce814b24a34bfb8a8c72d39b0e68
steinjun0 <1142308@naver.com>
2023-03-07 08:43:10 +0000

    IMentor, department nullable로 변경

--- src/src/interfaces/mentor.tsx
+++ src/src/interfaces/mentor.tsx
@@ -1,7 +1,7 @@
 export interface IMentor {
     company: string,
     divisIsPub: boolean,
-    department: string,
+    department?: string,
     job: string,
     nickname: string
     inJob: string,
