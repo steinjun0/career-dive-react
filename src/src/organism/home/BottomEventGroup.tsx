0e1af09c04eca8f25e17afb832c6cf94c6919a2a
steinjun0 <1142308@naver.com>
2023-03-07 08:25:43 +0000

    bottomeEventGroup 반응형 배치 추가

--- src/src/organism/home/BottomEventGroup.tsx
+++ src/src/organism/home/BottomEventGroup.tsx
@@ -2,7 +2,7 @@ import { colorCareerDiveBlue, colorTextLight, Flex, TextHeading5, VerticalFlex }
 import GuidelineMenteeBook from "assets/img/home/GuidelineMenteeBook.svg";
 import KakaotalkChannel from "assets/img/home/KakaotalkChannel.svg";
 import React, { DOMAttributes, ReactElement } from "react";
-import { StyledComponentProps } from '@mui/material';
+import { useMediaQuery, useTheme } from '@mui/material';
 import { MUIStyledCommonProps } from '@mui/system';
 
 
@@ -23,12 +23,15 @@ function EventCard({ children, ...props }: { children: ReactElement } & DOMAttri
 }
 
 function BottomEventGroup(props: MUIStyledCommonProps) {
+    const theme = useTheme()
+    const isDownMd = useMediaQuery(theme.breakpoints.down('md'));
+
     return (
-        <Flex sx={{ gap: '30px', ...props.sx }}>
+        <Flex sx={{ gap: '30px', flexDirection: isDownMd ? 'column' : 'row', ...props.sx }}>
             <EventCard onClick={() => window.open("https://www.notion.so/CBT-c57283dae1fe4602ad25f2b6ba0419aa")}>
                 <>
                     <VerticalFlex>
-                        <TextHeading5 color={colorTextLight}>서비스 정책 및 가이드라인</TextHeading5>
+                        <TextHeading5 color={colorTextLight} sx={{ wordBreak: 'keep-all' }}>서비스 정책 및 가이드라인</TextHeading5>
                         <TextHeading5 color={colorCareerDiveBlue}>멘티편</TextHeading5>
                     </VerticalFlex>
                     <VerticalFlex
@@ -41,7 +44,7 @@ function BottomEventGroup(props: MUIStyledCommonProps) {
             <EventCard onClick={() => window.open("https://pf.kakao.com/_xhHtxlxj")}>
                 <>
                     <VerticalFlex>
-                        <TextHeading5 color={'#F3C72B'}>카카오톡 채널 문의</TextHeading5>
+                        <TextHeading5 color={'#F3C72B'} sx={{ wordBreak: 'keep-all' }}>카카오톡 채널 문의</TextHeading5>
                     </VerticalFlex>
                     <VerticalFlex
                         style={{ justifyContent: 'end' }}>
