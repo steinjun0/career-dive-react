9e269f57e867446ed8cfdc2dafd2081fa53812f4
steinjun0 <1142308@naver.com>
2023-03-08 08:18:31 +0000

    EventGroup 반응형 폰트 변경

--- src/src/organism/home/EventGroup.tsx
+++ src/src/organism/home/EventGroup.tsx
@@ -1,4 +1,4 @@
-import { colorCareerDiveBlue, colorTextLight, defaultBoxShadow, Flex, TextHeading5, VerticalFlex } from 'util/styledComponent'
+import { colorCareerDiveBlue, colorTextLight, defaultBoxShadow, Flex, TextHeading5, TextSubtitle1, VerticalFlex } from 'util/styledComponent'
 import GuidelineMenteeBook from "assets/img/home/GuidelineMenteeBook.svg";
 import KakaotalkChannel from "assets/img/home/KakaotalkChannel.svg";
 import React, { DOMAttributes, ReactElement } from "react";
@@ -24,18 +24,26 @@ function EventCard({ children, ...props }: { children: ReactElement } & DOMAttri
 
 function EventGroup(props: MUIStyledCommonProps) {
     const theme = useTheme()
-    const isDownSm = useMediaQuery(theme.breakpoints.down('sm'));
+    const isDownHomeBreakPoint = useMediaQuery(theme.breakpoints.down(614));
 
     return (
-        <Flex sx={{ gap: '30px', flexDirection: isDownSm ? 'column' : 'row', ...props.sx }}>
+        <Flex sx={{ gap: '30px', flexDirection: isDownHomeBreakPoint ? 'column' : 'row', ...props.sx }}>
             <EventCard onClick={() => window.open("https://www.notion.so/CBT-c57283dae1fe4602ad25f2b6ba0419aa")}>
                 <>
-                    <VerticalFlex>
-                        <TextHeading5 color={colorTextLight} sx={{ wordBreak: 'keep-all' }}>서비스 정책 및 가이드라인</TextHeading5>
-                        <TextHeading5 color={colorCareerDiveBlue}>멘티편</TextHeading5>
-                    </VerticalFlex>
+                    {
+                        isDownHomeBreakPoint ?
+                            <VerticalFlex>
+                                <TextSubtitle1 color={colorTextLight} sx={{ wordBreak: 'keep-all' }}>서비스 정책 및 가이드라인</TextSubtitle1>
+                                <TextSubtitle1 color={colorCareerDiveBlue}>멘티편</TextSubtitle1>
+                            </VerticalFlex>
+                            :
+                            <VerticalFlex>
+                                <TextHeading5 color={colorTextLight} sx={{ wordBreak: 'keep-all' }}>서비스 정책 및 가이드라인</TextHeading5>
+                                <TextHeading5 color={colorCareerDiveBlue}>멘티편</TextHeading5>
+                            </VerticalFlex>
+                    }
                     <VerticalFlex
-                        style={{ justifyContent: 'end' }}>
+                        sx={{ justifyContent: 'end' }}>
                         <img src={GuidelineMenteeBook} alt="" />
                     </VerticalFlex>
                 </>
@@ -43,11 +51,19 @@ function EventGroup(props: MUIStyledCommonProps) {
 
             <EventCard onClick={() => window.open("https://pf.kakao.com/_xhHtxlxj")}>
                 <>
-                    <VerticalFlex>
-                        <TextHeading5 color={'#F3C72B'} sx={{ wordBreak: 'keep-all' }}>카카오톡 채널 문의</TextHeading5>
-                    </VerticalFlex>
+                    {
+                        isDownHomeBreakPoint ?
+                            <VerticalFlex>
+                                <TextSubtitle1 color={'#F3C72B'} sx={{ wordBreak: 'keep-all' }}>카카오톡 채널 문의</TextSubtitle1>
+                            </VerticalFlex>
+                            :
+                            <VerticalFlex>
+                                <TextHeading5 color={'#F3C72B'} sx={{ wordBreak: 'keep-all' }}>카카오톡 채널 문의</TextHeading5>
+                            </VerticalFlex>
+                    }
+
                     <VerticalFlex
-                        style={{ justifyContent: 'end' }}>
+                        sx={{ justifyContent: 'end' }}>
                         <img src={KakaotalkChannel} alt="" />
                     </VerticalFlex>
                 </>
