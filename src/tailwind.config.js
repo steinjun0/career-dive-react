72279ee23929005487a2c7a85c7e6f93b6bb45e2
steinjun0 <1142308@naver.com>
2023-03-08 07:57:05 +0000

    tailwind color 추가

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -2,6 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      'careerdive-blue': "#698CFF",
+      'bg-careerdive-blue': "#698CFF1a",
+      'careerdive-pink': "#E25D7D",
+      'bg-careerdive-pink': "#E25D7D1a",
+      'blue-gray': '#CFD6E0',
+      'text-light': "#5F6368",
+      'text-body': "#191919",
+      'text-disabled': "#D9D9D9",
+      'bg-gray-light': "#F8F8F8",
+      'bg-gray-medium': "#E6E6E6",
+      'bg-gray-dark': "#7C7C7C",
+      'success': "#B4CE16",
+    },
     extend: {},
   },
   plugins: [],
