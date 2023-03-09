5c91580218b60253c82f13a80eff90fc82f7fad2
steinjun0 <1142308@naver.com>
2023-03-09 14:59:28 +0000

    Footer 리팩토링 및 반응형 완료

--- src/src/organism/Footer.tsx
+++ src/src/organism/Footer.tsx
@@ -8,67 +8,21 @@ import {
   colorCareerDiveBlue,
   colorBackgroundGrayMedium,
   colorTextLight,
-  LinkNoDeco,
+  colorBackgroundGrayDark,
+  TextCaption,
+  colorTextBody,
 } from "util/styledComponent";
 
 import speechBubble from "../assets/icon/speechBubble.svg";
 
-const FooterFullWidthWrapper = styled(Flex)({
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-  width: '100%',
-  backgroundColor: 'white',
-});
 
-const FooterTop = styled(Flex)({
-  height: '172px',
-  width: '100%',
-  justifyContent: 'center',
-  alignItems: 'center',
-});
-
-const FooterBottom = styled(Flex)({
-  height: '46px',
-  width: '100%',
-  justifyContent: 'center',
-  borderTop: `1px solid ${colorBackgroundGrayMedium}`,
-});
-
-const FooterWrapper = styled(RowAlignCenterFlex)({
-  flexDirection: 'row',
-  justifyContent: 'start',
-  height: '100px',
-  width: '100%',
-  maxWidth: '1194px',
-});
-
-const FooterBottomWrapper = styled(RowAlignCenterFlex)({
-  justifyContent: 'space-between',
-  width: '100%',
-  maxWidth: '1194px',
-  fontSize: '12px',
-  color: colorTextLight,
-});
-
-const ColumnSet = styled(VerticalFlex)({
-  alignItems: 'start',
-  justifyContent: 'space-between',
-  width: '175px',
-  height: '96px',
-  fontSize: '14px',
-});
-
-const ColumnTitle = styled('span')({
-  fontWeight: '700',
-});
 
 const ContactCCudaButton = styled(RowAlignCenterFlex)({
   justifyContent: 'center',
   backgroundColor: colorCareerDiveBlue,
   borderRadius: '16px',
   height: '32px',
-  width: '194px',
+  width: '100%',
   marginLeft: 'auto',
   marginBottom: 'auto',
   color: 'white',
@@ -83,59 +37,138 @@ const SpeechBubbleIcon = styled('img')({
 
 const footerDisableUrl = ['/login', '/signup', '/mentor/register', '/session', '/review'];
 
+function SupportButton() {
+  return (
+    <ContactCCudaButton onClick={
+      () => {
+        window.open('http://pf.kakao.com/_xhHtxlxj/chat');
+      }
+    }>
+      <SpeechBubbleIcon src={speechBubble} alt='speech-bubble' />
+      문의하기
+    </ContactCCudaButton>
+  );
+}
+
+function FooterTop(props: { items: { title: string, value: { name: string, link: string; }[]; }[]; }) {
+  function Column(props: { title: string, value: { name: string, link: string; }[]; }) {
+    return (
+      <VerticalFlex sx={{ justifyContent: 'space-between', width: '120px', gap: '14px' }}>
+        <span style={{ fontWeight: 700, fontSize: '14px' }}>{props.title}</span>
+        {props.value.map((e, i) => {
+          return <span key={i} style={{ color: colorTextBody, fontSize: '14px', lineHeight: '24px' }}>{e.name}</span>;
+        })}
+      </VerticalFlex>
+    );
+  }
+  return (
+    <Flex sx={{ width: '100%', justifyContent: 'space-between', padding: '36px 16px ' }}>
+      <Flex sx={{ gap: '48px' }}>
+        {props.items.map((item, i) => {
+          return <Column key={i} title={item.title} value={item.value} />;
+        })}
+
+      </Flex>
+      <Flex sx={{ width: '133px' }}>
+        <SupportButton />
+      </Flex>
+
+    </Flex>
+  );
+}
+
+function MobileFooterTop(props: { items: { title: string, value: { name: string, link: string; }[]; }[]; }) {
+
+  function Row(props: { title: string, value: { name: string, link: string; }[]; }) {
+    return (
+      <Flex sx={{ justifyContent: 'space-between' }}>
+        <span style={{ fontWeight: 700, fontSize: '12px' }}>{props.title}</span>
+        <Flex sx={{ gap: '8px' }}>
+          {props.value.map((e, i) => {
+            return <TextCaption color={colorBackgroundGrayDark} key={i}>{e.name}</TextCaption>;
+          })}
+        </Flex>
+      </Flex>
+    );
+  }
+
+  return (
+    <VerticalFlex sx={{ color: colorBackgroundGrayDark, width: '100%', gap: '8px', padding: '16px' }}>
+      {props.items.map((item, i) => {
+        return <Row key={i} title={item.title} value={item.value} />;
+      })}
+      <SupportButton />
+    </VerticalFlex>
+  );
+}
+
+function FooterBottom() {
+  return (
+    <Flex sx={{
+      justifyContent: 'space-between', width: '100%', borderTop: `1px solid ${colorBackgroundGrayMedium}`,
+      fontSize: '12px', color: colorTextLight,
+      padding: '14px 16px',
+    }}>
+      <div>artisd.studio, 사업자 등록 번호: 876-22-01034, 대표자: 김인종</div>
+      <div>Copyright 2022, 커리어다이브, All Rights Reserved.</div>
+    </Flex>
+  );
+}
+
+function MobileFooterBottom() {
+  return (
+    <VerticalFlex sx={{ width: '100%', padding: '14px 16px', borderTop: `1px solid ${colorBackgroundGrayMedium}` }}>
+      <VerticalFlex sx={{
+        justifyContent: 'space-between',
+        fontSize: '12px', color: colorBackgroundGrayDark,
+
+      }}>
+        <div>artisd.studio, 사업자 등록 번호: 876-22-01034, 대표자: 김인종</div>
+        <div>Copyright 2022, 커리어다이브, All Rights Reserved.</div>
+      </VerticalFlex>
+    </VerticalFlex>
+  );
+}
 
 function Footer() {
   const { pathname } = useLocation();
   const theme = useTheme();
   const isDown730 = useMediaQuery(theme.breakpoints.down(730));
 
-
   return (
-    <Flex id='footer'>
+    <Flex id='footer' sx={{ justifyContent: 'center', width: '100%', backgroundColor: 'white' }}>
       {!footerDisableUrl.map((e) => pathname.includes(e)).includes(true) &&
-        <FooterFullWidthWrapper>
-          <FooterTop>
-            <FooterWrapper>
-              <ColumnSet>
-                <ColumnTitle>COMPANY</ColumnTitle>
-                <div>기업소개</div>
-                <div>공지사항</div>
-              </ColumnSet>
-              <ColumnSet>
-                <ColumnTitle>POLICY</ColumnTitle>
-                <div>이용약관</div>
-                <div>개인정보처리방침</div>
-              </ColumnSet>
-              <ColumnSet>
-                <ColumnTitle>SUPPORT</ColumnTitle>
-                <div style={{ cursor: 'pointer' }} onClick={() => {
-                  if (JSON.parse(localStorage.getItem('IsMentorMode')!)) {
-                    window.open('https://www.notion.so/CBT-30539442ad874299a12b6e727de3a506#a1ad0076d52e456fa46601d031fa34b3');
-                  } else {
-                    window.open('https://www.notion.so/CBT-c57283dae1fe4602ad25f2b6ba0419aa#c92e00a335c0443b986c70e77da7b5e1');
-                  }
-                }}>FAQ</div>
-                <div>커다센터</div>
-              </ColumnSet>
-              <ContactCCudaButton onClick={
-                () => {
-                  window.open('http://pf.kakao.com/_xhHtxlxj/chat');
-                }
-              }>
-                <SpeechBubbleIcon src={speechBubble} alt='speech-bubble' />
-                문의하기
-              </ContactCCudaButton>
-            </FooterWrapper>
-          </FooterTop>
-          <FooterBottom>
-            <FooterBottomWrapper>
-              <div>artisd.studio, 사업자 등록 번호: 876-22-01034, 대표자: 김인종</div>
-              <div>Copyright 2022, 커리어다이브, All Rights Reserved.</div>
-            </FooterBottomWrapper>
-          </FooterBottom>
-        </FooterFullWidthWrapper>
+        <VerticalFlex sx={{
+          justifyContent: 'center', alignValue: 'center', width: '100%',
+          maxWidth: 'calc(1192px + 32px)', backgroundColor: 'white',
+
+        }}>
+          {
+            isDown730 ?
+              <>
+                <MobileFooterTop
+                  items={[
+                    { title: "Company", value: [{ name: '기업소개', link: '' }, { name: '공지사항', link: '' }] },
+                    { title: "Policy", value: [{ name: '이용약관', link: '' }, { name: '개인정보처리방침', link: '' }] },
+                    { title: "Support", value: [{ name: '커다센터', link: '' }, { name: 'FAQ', link: '' }] },
+                  ]}
+                />
+                <MobileFooterBottom />
+              </> :
+              <>
+                <FooterTop
+                  items={[
+                    { title: "COMPANY", value: [{ name: '기업소개', link: '' }, { name: '공지사항', link: '' }] },
+                    { title: "POLICY", value: [{ name: '이용약관', link: '' }, { name: '개인정보처리방침', link: '' }] },
+                    { title: "SUPPORT", value: [{ name: '커다센터', link: '' }, { name: 'FAQ', link: '' }] },
+                  ]}
+                />
+                <FooterBottom />
+              </>
+          }
+        </VerticalFlex>
       }
-    </Flex>
+    </Flex >
 
   );
 }
