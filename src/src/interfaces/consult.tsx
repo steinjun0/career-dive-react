e2730c242b512c2152f131c05926c9a43d8b9ada
steinjun0 <1142308@naver.com>
2023-03-20 12:08:00 +0000

    create intefaces/consult

--- src/src/interfaces/consult.tsx
+++ src/src/interfaces/consult.tsx
@@ -1,1 +1,21 @@
-type TConsultStatus = 'approved' | 'created' | 'rejected' | 'done' | 'mentor_noshow' | 'mentee_noshow' | 'noshow';
+export type TConsultStatus = 'approved' | 'created' | 'rejected' | 'done' | 'mentor_noshow' | 'mentee_noshow' | 'noshow';
+export interface IConsult {
+    id: number,
+    date: Date,
+    startTime: Date,
+    endTime: Date,
+    menteeId: number,
+    status: TConsultStatus,
+    consultContentList: {
+        id: number,
+        name: string,
+        type: string;
+    }[],
+    company: string,
+    divisIsPub: boolean,
+    department?: string,
+    job: string,
+    nickname: string;
+    inJob: string,
+    duration: number,
+}
