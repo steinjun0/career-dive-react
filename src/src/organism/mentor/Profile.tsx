d8120464dfaf02b36373c72195353eee0ac2d8f5
steinjun0 <1142308@naver.com>
2023-03-22 06:54:08 +0000

    MobileMentorProfile 하단 bar mentor/[id] -> Profile.tsx로 이동

--- src/src/organism/mentor/Profile.tsx
+++ src/src/organism/mentor/Profile.tsx
@@ -23,6 +23,50 @@ import React from "react";
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 import CustomTogglebutton from "component/button/CustomToggleButton";
 
+function MobileBottomBar({ isFavorite, setIsFavorite, id }: { isFavorite: boolean, setIsFavorite: React.Dispatch<React.SetStateAction<boolean>>, id: number; }) {
+    const navigater = useNavigate();
+
+    return <Flex
+        style={{
+            position: 'fixed',
+            left: 0,
+            zIndex: 10,
+            bottom: 0,
+            padding: '8px 16px',
+            height: '56px',
+            gap: '8px',
+            backgroundColor: 'white',
+            width: '100vw',
+            filter: 'drop-shadow(0px -20px 40px rgba(130, 130, 130, 0.1))'
+        }}
+    >
+        <CustomTogglebutton
+            value='check'
+            selected={isFavorite}
+            onChange={() => {
+                setIsFavorite((prev) => !prev);
+            }}>
+            <BookmarkBorderIcon />
+        </CustomTogglebutton>
+        <Button
+            disableElevation
+            disableRipple
+            sx={{
+                width: '100%',
+                backgroundColor: colorCareerDiveBlue,
+                color: 'white',
+                borderRadius: '8px',
+                '&:hover': { backgroundColor: colorCareerDiveBlue },
+            }}
+            onClick={() => { navigater(`/mentee/mentor/${id}/request`); }}
+        >
+            <TextSubtitle1>
+                상담 신청
+            </TextSubtitle1>
+        </Button>
+    </Flex>;
+}
+
 function MentorProfile({ name = '', description = '', inService = true, id = -1 }) {
     const theme = useTheme();
     const isDownMd = useMediaQuery(theme.breakpoints.down('md'));
@@ -49,42 +93,51 @@ function MentorProfile({ name = '', description = '', inService = true, id = -1 
                 </VerticalFlex>
             </Flex>
             {
-                !isDownMd &&
-                <Flex style={{
-                    marginLeft: 'auto',
-                    height: '40px',
-                    gap: '12px',
-                    justifyContent: 'space-between'
-                }}>
-                    <CustomTogglebutton
-                        value='check'
-                        selected={isFavorite}
-                        onChange={() => {
-                            setIsFavorite((prev) => !prev);
-                        }}>
-                        <BookmarkBorderIcon />
-                    </CustomTogglebutton>
-                    {!location.pathname.includes('request') &&
-                        <Button
-                            disableElevation
-                            disableRipple
-                            sx={{
-                                backgroundColor: colorCareerDiveBlue,
-                                color: 'white',
-                                borderRadius: '8px',
-                                '&:hover': { backgroundColor: colorCareerDiveBlue },
-                                width: '100px'
-                            }}
-                            onClick={() => { navigater(`/mentee/mentor/${id}/request`); }}
-                        >
-                            <TextSubtitle1>
-                                상담 신청
-                            </TextSubtitle1>
-                        </Button>
-                    }
-                </Flex>
+                !isDownMd ?
+                    <Flex style={{
+                        marginLeft: 'auto',
+                        height: '40px',
+                        gap: '12px',
+                        justifyContent: 'space-between'
+                    }}>
+                        <CustomTogglebutton
+                            value='check'
+                            selected={isFavorite}
+                            onChange={() => {
+                                setIsFavorite((prev) => !prev);
+                            }}>
+                            <BookmarkBorderIcon />
+                        </CustomTogglebutton>
+                        {
+                            !location.pathname.includes('request') &&
+                            <Button
+                                disableElevation
+                                disableRipple
+                                sx={{
+                                    backgroundColor: colorCareerDiveBlue,
+                                    color: 'white',
+                                    borderRadius: '8px',
+                                    '&:hover': { backgroundColor: colorCareerDiveBlue },
+                                    width: '100px'
+                                }}
+                                onClick={() => { navigater(`/mentee/mentor/${id}/request`); }}
+                            >
+                                <TextSubtitle1>
+                                    상담 신청
+                                </TextSubtitle1>
+                            </Button>
+                        }
+                    </Flex> :
+                    (
+                        !location.pathname.includes('request') &&
+                        <MobileBottomBar
+                            isFavorite={isFavorite}
+                            setIsFavorite={setIsFavorite}
+                            id={id}
+                        />
+                    )
             }
-        </Flex>
+        </Flex >
     );
 }
 
