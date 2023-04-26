ccdddcebfe0607bf6669590d9516aaa725974296
steinjun0 <1142308@naver.com>
2023-04-26 13:46:12 +0000

    update SearchBar input placeholder color

--- src/src/organisms/search/SearchBar.tsx
+++ src/src/organisms/search/SearchBar.tsx
@@ -1,6 +1,14 @@
 import { Flex, VerticalFlex } from "util/styledComponent";
 import React, { useState } from "react";
 import SearchIcon from "assets/icon/search.svg";
+import { styled } from "@mui/material";
+
+const ColoredInput = styled('input')({
+  '::placeholder': {
+    color: '#BDBDBD'
+  }
+});
+
 export default function SearchTags() {
   return <VerticalFlex>
     <Flex sx={{
@@ -17,11 +25,11 @@ export default function SearchTags() {
       </Flex>
       <Flex sx={{ padding: '16px 24px' }}>
         <span style={{ fontWeight: 'bold', lineHeight: '24px', fontSize: '14px', marginRight: '14px' }}>직무</span>
-        <input type="text" placeholder="회사를 입력하세요" style={{ width: '122px' }} />
+        <ColoredInput type="text" placeholder="회사를 입력하세요" style={{ width: '122px' }} />
       </Flex>
       <Flex sx={{ padding: '16px 24px' }}>
         <span style={{ fontWeight: 'bold', lineHeight: '24px', fontSize: '14px', marginRight: '14px' }}>직무</span>
-        <input type="text" placeholder="태그를 입력하세요" style={{ width: '122px' }} />
+        <ColoredInput type="text" placeholder="태그를 입력하세요" style={{ width: '122px' }} />
       </Flex>
       <img src={SearchIcon} alt="search" style={{ width: '36px', height: '36px', margin: '10px', cursor: 'pointer' }}
         onClick={() => { }}
