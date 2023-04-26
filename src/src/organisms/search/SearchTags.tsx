ad58bb5865f8fc5f9c3624654a9a9f0f03045b20
steinjun0 <1142308@naver.com>
2023-04-26 09:00:57 +0000

    update SearchTags

--- src/src/organisms/search/SearchTags.tsx
+++ src/src/organisms/search/SearchTags.tsx
@@ -1,9 +1,11 @@
-import { Flex } from "util/styledComponent";
+import { Flex, colorBackgroundGrayMedium } from "util/styledComponent";
 import React, { useState } from "react";
 import RoundedGraySelect from "component/select/RoundedGraySelect";
+import CustomTogglebutton from "component/button/CustomToggleButton";
+import { ToggleButtonGroup } from "@mui/material";
 export default function SearchBar() {
   const [sector, setSector] = useState<string>();
-  return <Flex sx={{ gap: '16px', marginTop: '35px' }}>
+  return <Flex sx={{ gap: '16px', marginTop: '35px', justifyContent: 'center', alignItems: 'center' }}>
     <RoundedGraySelect
       texts={['반도체', 'IT']}
       label="업종"
@@ -15,24 +17,24 @@ export default function SearchBar() {
       handleChange={(value) => { setSector(value.target.value as string); }}
     />
     <RoundedGraySelect
-      texts={['반도체', 'IT']}
-      label="업종"
+      texts={['좋음', '나쁨']}
+      label="만족도"
       handleChange={(value) => { setSector(value.target.value as string); }}
     />
-    |
+    <div style={{ borderRight: `2px solid ${colorBackgroundGrayMedium}`, height: '20px' }} />
+
+    <CustomTogglebutton value={20}>일반</CustomTogglebutton>
+    <CustomTogglebutton value={20}>프리미엄</CustomTogglebutton>
+
+
     <RoundedGraySelect
       texts={['반도체', 'IT']}
       label="업종"
       handleChange={(value) => { setSector(value.target.value as string); }}
     />
     <RoundedGraySelect
-      texts={['반도체', 'IT']}
-      label="업종"
-      handleChange={(value) => { setSector(value.target.value as string); }}
-    />
-    <RoundedGraySelect
-      texts={['반도체', 'IT']}
-      label="업종"
+      texts={['면접 준비', '시험 준비']}
+      label="상담 내용"
       handleChange={(value) => { setSector(value.target.value as string); }}
     />
   </Flex>;
