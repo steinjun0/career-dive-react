1a928477ef6b243773a42c497daa2786e9235dc9
steinjun0 <1142308@naver.com>
2023-04-27 08:21:20 +0000

    update register pages with registerTemplate

--- src/src/pages/mentor/register2.tsx
+++ src/src/pages/mentor/register2.tsx
@@ -5,34 +5,34 @@ import paperFile from 'assets/img/paperFile.svg';
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 import issueStepModalObject from "organisms/mentor/register/IssueStepModal";
+import RegisterTemplate from "organisms/mentor/register/RegisterTemplate";
 
 const InfoModal = withReactContent(Swal);
 
 export default function MentorRegister2() {
-  return <Flex sx={{ justifyContent: 'center', alignItems: 'center', minHeight: 'calc(100vh - 80px - 80px)' }}>
-    <VerticalFlex sx={{ alignItems: 'center', width: '378px' }}>
-      <TextHeading6 sx={{ width: '100%' }}>
-        멘토 등록을 위해<br />
-        <span style={{ color: colorCareerDivePink }}>건강보험자격득실확인서</span>를<br />
-        먼저 준비해주세요<br />
-      </TextHeading6>
-      <img src={paperFile} alt="서류뭉치" style={{ width: '120px', height: '120px', margin: '72px 0' }} />
-      <Flex sx={{ gap: '16px', width: '100%' }}>
-        <BasicButton sx={{ width: '100%', height: '48px' }} type="gray"
-          onClick={() => {
-            InfoModal.fire(issueStepModalObject);
-          }}
-        >
-          <TextSubtitle1 sx={{ color: colorTextLight }}>
-            서류 발급
-          </TextSubtitle1>
-        </BasicButton>
-        <BasicButton sx={{ width: '100%', height: '48px' }} type="pink">
-          <TextSubtitle1>
-            등록 시작
-          </TextSubtitle1>
-        </BasicButton>
-      </Flex>
-    </VerticalFlex>
-  </Flex>;
+
+  return <RegisterTemplate>
+    <TextHeading6 sx={{ width: '100%' }}>
+      멘토 등록을 위해<br />
+      <span style={{ color: colorCareerDivePink }}>건강보험자격득실확인서</span>를<br />
+      먼저 준비해주세요<br />
+    </TextHeading6>
+    <img src={paperFile} alt="서류뭉치" style={{ width: '120px', height: '120px', margin: '72px 0' }} />
+    <Flex sx={{ gap: '16px', width: '100%' }}>
+      <BasicButton sx={{ width: '100%', height: '48px' }} type="gray"
+        onClick={() => {
+          InfoModal.fire(issueStepModalObject);
+        }}
+      >
+        <TextSubtitle1 sx={{ color: colorTextLight }}>
+          서류 발급
+        </TextSubtitle1>
+      </BasicButton>
+      <BasicButton sx={{ width: '100%', height: '48px' }} type="pink">
+        <TextSubtitle1>
+          등록 시작
+        </TextSubtitle1>
+      </BasicButton>
+    </Flex>
+  </RegisterTemplate>;
 }
