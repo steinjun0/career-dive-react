b4f88b966a599fc65037d59bb172633480c89662
steinjun0 <1142308@naver.com>
2023-04-29 07:26:49 +0000

    create interface IMentorRegister

--- src/src/interfaces/mentor.tsx
+++ src/src/interfaces/mentor.tsx
@@ -1,3 +1,6 @@
+import { FileWithPath } from "react-dropzone";
+import { Job, Sector } from "./job";
+
 export interface IMentor {
     company: string,
     divisIsPub: boolean,
@@ -10,4 +13,18 @@ export interface IMentor {
     rating: number,
     tags: string[],
     userId: number,
+}
+
+export interface IMentorRegisterData {
+    birth: string,
+    careerFile: FileWithPath | null,
+    company: string,
+    divisIsPub: boolean,
+    sector: Sector,
+    job: Job,
+    jobInComp: string,
+    department: string,
+    tags: string[],
+    consultList: string[],
+    typeList: string[],
 }
